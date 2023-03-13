7783156e1e081d42e095e5f68e63ba78a47775e7
David Morales <david_morales95@outlook.com>
2023-03-13 17:00:11 -0600

    se agregan archivos de la tarea

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Motor-de-busqueda
+# Motor de busqueda
+
+## Descripcion
+
+Esta aplicacion te ayuda a buscar libros, asi como tambien cuenta con la funcion de poder crear una cuenta y guardar libros en los cuales puedas estar interesado
+
+## Indice
+
+- [Instalacion](#Instalacion)
+- [Uso](#Uso)
+- [Colaboradores](#Colaboradores)
+- [Licencia](#license)
+- [Pruebas](#Pruebas)
+- [Github](#githubUser)
+- [Email](#userEmail)
+
+## Instalacion
+
+Npm i, Npm run develop
+
+## Uso
+
+Puedes utilizar esta aplicacion para guardar libros que estes interesado en leer
+
+## Demostracion
+
+![imagen de la pagina](https://user-images.githubusercontent.com/111394587/224850390-3d4bbaa8-d677-4fbb-983b-0bb3f33301cd.png)
+![imagen de la busqueda](https://user-images.githubusercontent.com/111394587/224850445-8c0e04ee-dcc3-4baa-ba25-93cd56a50713.png)
+![imagen del loginfallido](https://user-images.githubusercontent.com/111394587/224850507-828ff216-82b8-42c8-9ca4-2b711b193d22.png)
+
+## Colaboradores
+
+David Morales
+
+## Licencia
+
+    Licencia: MIT
+
+![Github license](https://img.shields.io/badge/license-MIT-green.svg)
+
+\*[Licencia](#license)
+
+## Pruebas
+
+N/A
+
+## GitHub
+
+[Davidmome](https://github.com/Davidmome/)
+
+## Email
+
+david_morales95@outlook.com
---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,98 @@
+// consulte SignupForm.js para obtener comentarios
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import Auth from "../utils/auth";
+import { useMutation } from "@apollo/client";
+import { loginUser } from "../utils/API";
+import { LOGIN_USER } from "../utils/mutations";
+
+const LoginForm = () => {
+  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
+  const [loginUsr, { error, data }] = useMutation(LOGIN_USER);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // verifique si el formulario tiene todo (según los documentos de react-bootstrap)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const { data } = await loginUsr({ variables: { ...userFormData } });
+      console.log("loginUsr:", data);
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert
+          dismissible
+          onClose={() => setShowAlert(false)}
+          show={showAlert}
+          variant="danger"
+        >
+          Something went wrong with your login credentials!
+        </Alert>
+        <Form.Group>
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your email"
+            name="email"
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Email is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            name="password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={!(userFormData.email && userFormData.password)}
+          type="submit"
+          variant="success"
+        >
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default LoginForm;
